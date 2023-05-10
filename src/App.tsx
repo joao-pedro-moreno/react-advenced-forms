59a77100370bc3269aa91bd013b922d3b6b3b77c
João Pedro Moreno <devmoreno2003@gmail.com>
2023-05-09 23:54:53 -0300

    Transformação de valores

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /*
   * To-do
   *
-  * [ ] Validação / transformação
+  * [X] Validação / transformação
   * [ ] Field Arrays
   * [ ] Upload de arquivos
   * [ ] Composition Pattern
@@ -19,11 +19,23 @@ import { zodResolver } from '@hookform/resolvers/zod'
 // Representação da estrutura de dados esperada como dados do formulário
 const createUserFormSchema = z.object({
   // string() -> Informa qual o tipo do valor esperado
+  name: z.string()
+    .nonempty('O nome é obrigatório')
+    // Transforma o valor recebido
+    .transform(name => {
+      return name.trim().split(' ').map(word => {
+        return word[0].toLocaleUpperCase().concat(word.substring(1))
+      }).join(' ')
+    }),
   email: z.string()
     // Informa que o campo é obrigatório
     .nonempty('O e-mail é obrigatório')
     // Valida a informação recebida com Regex
-    .email('Formato de e-mail inválido'),
+    .email('Formato de e-mail inválido')
+    // Permite a criação de validações extras
+    .refine(email => {
+      return email.endsWith('@gmail.com')
+    }, 'Domínio de e-mail não permitido'),
   password: z.string()
     // Define tamanho mínimo
     .min(6, 'A senha precisa de no mínimo 6 caracteres')
@@ -54,15 +66,25 @@ function App() {
       {/* A função 'handleSubmit' é executada quando o formulário é enviado e recebe como parametro a função que irá lidar com os dados */}
       <form action="" onSubmit={handleSubmit(createUser)}>
         <div>
+          <label htmlFor="name">Nome</label>
+          <input
+            type="name"
+            id="name"
+            // O input é registrado com o seu atributo 'name'
+            {...register('name')}
+          />
+
+          {/* Caso tiver algum erro de validação ele renderiza na tela com a mensagem */}
+          {errors.name && <span>{errors.name.message}</span>}
+        </div>
+        <div>
           <label htmlFor="email">E-mail</label>
           <input
             type="email"
             id="email"
-            // O input é registrado com o seu 'name'
             {...register('email')}
           />
 
-          {/* Caso tiver algum erro de validação ele renderiza na tela com a mensagem */}
           {errors.email && <span>{errors.email.message}</span>}
         </div>
 
